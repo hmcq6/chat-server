a4367457aff225219721a84a606440a1dbb23242
Hassan McKusick <Hassan.McKusick@gmail.com>
2018-06-24 16:03:11 -0400

    Unlink RedisInstance and RedisSubscriber, export them as named exports/imports

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,32 @@
 const RedisClient = require('ioredis'),
-      RedisInstance = require('../lib/redis-instance'),
-      RedisSubscriber = require('../lib/redis-subscriber');
+      { RedisInstance } = require('../lib/redis-instance'),
+      { RedisSubscriber } = require('../lib/redis-subscriber');
 
 
 const indexRoute = (ws, { params: { channel, user } }) => {
   const redisClient = new RedisInstance(),
-        redisSubscriber = new RedisSubscriber(new RedisClient(), 'KEAs', `__keyspace@0__:${ channel }:messages`);
+        redisSubscriber = new RedisSubscriber(new RedisClient(), 'Kgs', `__keyspace@0__:${ channel }:messages`);
 
   redisSubscriber.addListener(
     'message',
     `${ channel }:messages`,
-    function(wsChannel, redisChannel, message) {
+    function(redisChannel, message) {
       const [ _, ...channelParts ] = redisChannel.split(':');
 
       redisClient.notifyClient(ws, channelParts.join(':'))
-    }.bind(null, channel)
+    }
   );
 
   redisClient.notifyClient(ws, `${ channel }:messages`);
 
-  ws.on('message', function(message) {
+  ws.on('message', function(JSONMessage) {
+    const { message, sentAt } = JSON.parse(JSONMessage);
     redisClient.redis.sadd(
       `${ channel }:messages`,
       JSON.stringify({
         from: user,
-        message
+        message,
+        sentAt
       })
     )
   });
